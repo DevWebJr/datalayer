12c64bee1c33426deb15f5e5f8a0cfb432bfb672
DevWebJr <devwebjr@gmail.com>
2022-11-28 21:01:15 +0100

    initialize project and create classes

---
+++ src/main/java/com/carlib/datalayer/model/Category.java
@@ -0,0 +1,32 @@
+package com.carlib.datalayer.model;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "categorie")
+public class Category {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "categorie_id")
+    private int CategoryId;
+
+    @Column(name = "nom")
+    private String name;
+
+    public int getCategoryId() {
+        return CategoryId;
+    }
+
+    public void setCategoryId(int categoryId) {
+        CategoryId = categoryId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
